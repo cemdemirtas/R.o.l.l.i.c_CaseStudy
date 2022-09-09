ce66037d4b6d1a47d0e3f03227b69f52f7b457d3
Cem Demirtas <97852548+cemdemirtas@users.noreply.github.com>
2022-09-09 13:02:31 +0300

    First Commit
    Assets has organized

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    
+    public enum GameState { StartScreen, GamePlay, Failed, Complete,Win }
+
+    public static Action OnLevelCompleted;
+    public static Action OnLevelStarted;
+    public static Action OnLevelRestart;
+    public static Action OnLevelNext;
+    public static Action OnContinueLevel;
+
+   [SerializeField] public static GameState gameState;
+
+    private void OnEnable()
+    {
+        OnLevelStarted += StartLevel;
+        OnLevelNext += NextLevel;
+        OnLevelCompleted += CompleteLevel;
+        OnContinueLevel += ContinueLevel;
+        OnLevelRestart += FailedLevel;
+
+    }
+    private void OnDisable()
+    {
+        OnLevelStarted -= StartLevel;
+        OnLevelCompleted -= CompleteLevel;
+        OnContinueLevel -= ContinueLevel;
+        OnLevelRestart -= FailedLevel;
+        OnLevelNext -= NextLevel;
+
+    }
+
+    private void StartLevel()
+    {
+        gameState = GameState.GamePlay;
+    }
+
+    private void FailedLevel()
+    {
+        gameState = GameState.Failed;
+    }
+
+    private void NextLevel()
+    {
+        gameState = GameState.Win;
+    }
+
+
+    private void ContinueLevel()
+    {
+        gameState = GameState.GamePlay;
+    } 
+    private void CompleteLevel()
+    {
+        gameState = GameState.Complete;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float verticalSpeed;
+
+    [SerializeField]
+    private float horizontalSpeed;
+
+    private float verticalMoveSpeed;
+    private float horizontalMoveSpeed;
+
+    [Header("Movement Clamps")]
+    [SerializeField]
+    private float maxX;
+    [SerializeField]
+    private float minX;
+
+    private float progress;
+
+    [Header("References")]
+    [SerializeField]
+    private Rigidbody rigidbodyComponent;
+
+    [SerializeField]
+    private GameObject startLine;
+    [SerializeField]
+    private GameObject finishLine;
+
+    private bool canMove;
+
+    public static PlayerMovement playerMovement;
+    private void Awake()
+    {
+        if (playerMovement == null)
+        {
+            playerMovement = this;
+        }
+
+        rigidbodyComponent = GetComponent<Rigidbody>();
+
+        Vector3 startPosition = new Vector3(startLine.transform.position.x,
+            transform.position.y, startLine.transform.position.z);
+        transform.position = startPosition;
+    }
+
+    void Start()
+    {
+        horizontalMoveSpeed = horizontalSpeed;
+        verticalMoveSpeed = verticalSpeed;
+    }
+    private void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX),
+            transform.position.y, transform.position.z);
+
+        progress = (transform.position.z - startLine.transform.position.z) /
+            (finishLine.transform.position.z - startLine.transform.position.z);
+
+        UserInterfaceManager.userInterfaceManager.ShowProgress(progress);
+    }
+
+    private void FixedUpdate()
+    {
+        float xMov = 0;
+
+        if (IsPlayerCanMove())
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                if (Physics.Raycast(ray, out RaycastHit raycastHit))
+                {
+                    xMov = (raycastHit.point.x - transform.position.x) * 5;
+                }
+            }
+
+            rigidbodyComponent.velocity = new Vector3(xMov * horizontalMoveSpeed,
+                rigidbodyComponent.velocity.y, verticalMoveSpeed);
+        }
+        else
+        {
+            rigidbodyComponent.velocity = Vector3.zero;
+        }
+    }
+    public void CantMove()
+    {
+        //Debug.Log("CantMove Player");
+        canMove = false;
+    }
+    public void CanMove()
+    {
+        //Debug.Log("CanMove Player");
+        canMove = true;
+    }
+    public bool IsPlayerCanMove()
+    {
+        return canMove;
+    }
+}
